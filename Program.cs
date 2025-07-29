4d9025960d94a732691136ad7d97dd88a4bb568d
BotelhoDavid <david.gbotelho@gmail.com>
2025-07-28 23:19:38 -0300

    feat: add testes das classes Nokie e IPhone na classe Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("SmartPhone Nokia");
+Nokia nokia = new Nokia(numero: "11 11111-1111", modelo: "A35", imei: "1315489642337438738438", memoria: 125);
+nokia.Ligar();
+nokia.InstalarAplicativo("Zap Zap");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("SmartPhone Iphone");
+Iphone iphone = new Iphone(numero: "22 22222-2222", modelo: "21", imei: "3949489642337438738438", memoria: 125);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
